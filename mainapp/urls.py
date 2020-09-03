4d82fdcfdd5121a1b4ca01667428b432c10622b1
shubham07shastri70 <51118729+shubham07shastri70@users.noreply.github.com>
2020-09-04 00:13:00 +0530

    Add files via upload

---
+++ mainapp/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.urls import path
+from mainapp import views
+urlpatterns = [
+    path('',views.general,name='general'),
+    path('login',views.login,name="login"),
+    path('send',views.send,name="send"),
+    path('sendmessage',views.send_message,name="send"),
+    path('greeting',views.send_greetings,name="send"),
+    path('withdraw', views.withdraw,name="withdraw"),
+    path('send_greetings', views.send_greetings,name="send_greetings")
+    
+]
